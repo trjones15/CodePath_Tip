c94cd98d439865c23509ed1c0748acdb2fe34f09
Trevor Jones <trjones15@users.noreply.github.com>
2016-12-30 00:12:53 -0700

    Final
    Should work... but none of the optional parts accomplished yet

---
+++ Tip_Calculator.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html>
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+<title>Tip Calculator</title>
+<body>
+	<div class="Beginning">
+		<h2>Tip Calculator</h2>
+		<br>
+		<form method="get">
+			<div<?php
+				if(isset($_GET["subtotal"]) && !empty($_GET["subtotal"]) 
+					&& is_numeric($_GET["subtotal"]) && $_GET["subtotal"] >= 0){
+					
+				} else {
+					echo ' class="error"';
+				}
+				?>>
+			Bill subtotal: $<input type="text" name="subtotal" size="7" <?php 
+			if(isset($_GET["subtotal"]) && !empty($_GET["subtotal"])
+				&& is_numeric($_GET["subtotal"]) && $_GET["subtotal"] >= 0){
+					echo "value=".$_GET["subtotal"];
+				}else{
+					echo 'value=0';
+					
+				}
+				?>></div><br><br>
+			Tip percentage:<br><br>
+			<?php
+			for ($i = 0; $i < 3; $i++) {
+				if(isset($_GET["percent"]) && (10+($i*5))==$_GET["percent"]) {
+					echo '<input type="radio" name="percent" checked value="' , 10 + ($i*5) , '"> ' , 10 + ($i*5) , '% 
+				';} else {
+				echo '<input type="radio" name="percent" value="' , 10 + ($i*5) , '"> ' , 10 + ($i*5) , '% 
+				';}
+			}
+			?><br><br>
+			
+			<div class="Sbutton">
+				<input type="submit" value="Submit">
+			</div>
+			<?php
+			if(isset($_GET["subtotal"]) && !empty($_GET["subtotal"]) && isset($_GET["percent"])){
+				if(is_numeric($_GET["subtotal"]) && $_GET["subtotal"] >= 0){
+					$subtotal = (double)$_GET["subtotal"];
+					$percent = ((double)$_GET["percent"])/100;
+					$tip = $subtotal * $percent;
+					$total = $subtotal + $tip;
+					$tip = number_format($tip, 2, '.', ',');
+					$total = number_format($total, 2, '.', ',');
+					echo '<div class="Inside">
+						Tip: $' . $tip .'<br>
+						Total: $'. $total . '
+					</div>';
+				}
+			}
+			?>
+	</div>
+</body>
+</html>
