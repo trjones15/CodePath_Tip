83a8cf6467c14f5039e2db6e1fe1ffb449d90f91
Trevor Jones <trjones15@users.noreply.github.com>
2017-01-06 18:21:24 -0700

    Added Functionality

--- Tip_Calculator.php
+++ Tip_Calculator.php
@@ -18,7 +18,7 @@
 						echo ' class="error"';
 					}
 					?>>
-				Bill subtotal: $<input type="text" name="subtotal" size="7" <?php 
+				Bill subtotal: $<input type="text" name="subtotal" size="5" <?php 
 				if(isset($_GET["subtotal"]) && !empty($_GET["subtotal"])
 					&& is_numeric($_GET["subtotal"]) && $_GET["subtotal"] >= 0){
 						echo "value=".$_GET["subtotal"];
@@ -27,6 +27,21 @@
 						
 					}
 					?>></div><br><br>
+				<div<?php
+					if(isset($_GET["percent"])){
+						if($_GET["percent"] ==3){
+							if(isset($_GET["custom"]) 
+								&& is_numeric($_GET["custom"]) && $_GET["custom"] >= 0){
+									
+								}
+								else {
+									echo ' class="error"';
+								}
+						}
+					} else {
+						echo ' class="error"';
+					}
+				?>>
 				Tip percentage:<br><br>
 				<?php
 				for ($i = 0; $i < 3; $i++) {
@@ -36,24 +51,96 @@
 					echo '<input type="radio" name="percent" value="' , 10 + ($i*5) , '"> ' , 10 + ($i*5) , '% 
 					';}
 				}
-				?><br><br>
+				?><br>
+				<input type="radio" name="percent" <?php if(3==$_GET["percent"]){echo 'checked ';}?>value="3"> Custom: <input type="text" name="custom" size="5" <?php 
+				if(isset($_GET["custom"]) && !empty($_GET["custom"])
+					&& is_numeric($_GET["custom"]) && $_GET["custom"] >= 0){
+						echo "value=".$_GET["custom"];
+					}else{
+						echo 'value=0';
+						
+					}?>> %<br>
+				</div><br>
+				
+				<div<?php
+					if(isset($_GET["people"]) && !empty($_GET["people"]) 
+						&& is_numeric($_GET["people"]) && $_GET["people"] >= 0){
+						
+					} else {
+						echo ' class="error"';
+					}
+					?>>
+				Split: <input type="text" name="people" size="5" <?php 
+				if(isset($_GET["people"]) && !empty($_GET["people"])
+					&& is_numeric($_GET["people"]) && $_GET["people"] >=0) {
+						echo "value=".$_GET["people"];
+					}else{
+						echo 'value=1';
+					}
+				?>> person(s)</div><br>
 				
 				<div class="Sbutton">
 					<input type="submit" value="Submit">
 				</div>
+				
 				<?php
-				if(isset($_GET["subtotal"]) && !empty($_GET["subtotal"]) && isset($_GET["percent"])){
+				if(isset($_GET["subtotal"]) && !empty($_GET["subtotal"]) && isset($_GET["percent"])&&isset($_GET["people"]) && !empty($_GET["people"])
+								&& is_numeric($_GET["people"]) && $_GET["people"] >0){
 					if(is_numeric($_GET["subtotal"]) && $_GET["subtotal"] >= 0){
-						$subtotal = (double)$_GET["subtotal"];
-						$percent = ((double)$_GET["percent"])/100;
-						$tip = $subtotal * $percent;
-						$total = $subtotal + $tip;
-						$tip = number_format($tip, 2, '.', ',');
-						$total = number_format($total, 2, '.', ',');
-						echo '<div class="Inside">
-							Tip: $' . $tip .'<br>
-							Total: $'. $total . '
-						</div>';
+						if($_GET["percent"] ==3){
+							if(isset($_GET["custom"]) 
+								&& is_numeric($_GET["custom"]) && $_GET["custom"] >= 0){
+							$subtotal = (double)$_GET["subtotal"];
+							if($_GET["percent"] == 3){
+								$percent = ((double)$_GET["custom"])/100;
+							} else {
+								$percent = ((double)$_GET["percent"])/100;
+							}
+							$tip = $subtotal * $percent;
+							$total = $subtotal + $tip;
+							$tipF = number_format($tip, 2, '.', ',');
+							$totalF = number_format($total, 2, '.', ',');
+							echo '<div class="Inside">
+								Tip: $' . $tipF .'<br>
+								Total: $'. $totalF;
+								if(isset($_GET["people"]) && !empty($_GET["people"])
+									&& is_numeric($_GET["people"]) && $_GET["people"] >1) {
+										$split = (int)$_GET["people"];
+										$adjustedTip = $tip/$split;
+										$adjustedTotal = $total/$split;
+										$adjustedTipF = number_format($adjustedTip, 2, '.', ',');
+										$adjustedTotalF = number_format($adjustedTotal, 2, '.', ',');
+										echo '<br><br>Tip each: $'. $adjustedTipF.'<br>
+										Total each: $' . $adjustedTotalF;
+									}
+							echo '</div>';
+							}
+						}else{
+							$subtotal = (double)$_GET["subtotal"];
+							if($_GET["percent"] == 3){
+								$percent = ((double)$_GET["custom"])/100;
+							} else {
+								$percent = ((double)$_GET["percent"])/100;
+							}
+							$tip = $subtotal * $percent;
+							$total = $subtotal + $tip;
+							$tipF = number_format($tip, 2, '.', ',');
+							$totalF = number_format($total, 2, '.', ',');
+							echo '<div class="Inside">
+								Tip: $' . $tipF .'<br>
+								Total: $'. $totalF;
+								if(isset($_GET["people"]) && !empty($_GET["people"])
+									&& is_numeric($_GET["people"]) && $_GET["people"] >1) {
+										$split = (int)$_GET["people"];
+										$adjustedTip = $tip/$split;
+										$adjustedTotal = $total/$split;
+										$adjustedTipF = number_format($adjustedTip, 2, '.', ',');
+										$adjustedTotalF = number_format($adjustedTotal, 2, '.', ',');
+										echo '<br><br>Tip each: $'. $adjustedTipF.'<br>
+										Total each: $' . $adjustedTotalF;
+									}
+							echo '</div>';
+						}
 					}
 				}
 				?>
